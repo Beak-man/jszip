f3085fc9d5f40078bede626895af450fe67f4878
Miguel Del Castillo <migueldelcastillo@live.com>
2017-08-24 13:50:35 -0500

    Removed version number from copyright notice

--- dist/jszip.js
+++ dist/jszip.js
@@ -1,5 +1,5 @@
 /*!
-JSZip v3.1.3 - A Javascript class for generating and reading zip files
+JSZip - A Javascript class for generating and reading zip files
 <http://stuartk.com/jszip>
 (c) 2009-2016 Stuart Knightley <stuart [at] stuartk.com>
 Dual licenced under the MIT license or GPLv3. See https://raw.github.com/Stuk/jszip/master/LICENSE.markdown.
